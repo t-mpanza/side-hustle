ed18137977e981e3ba29498ab806ef072b687ec4
t-mpanza <theolusmpanza@gmail.com>
2025-10-06 10:34:10 +0000

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shortie',
+  base: '/side-hustle',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
